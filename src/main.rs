6bf6f7f096dc04a4f611015233ae122f310724c6
Kaede Fujisaki <kaede@hexe.net>
2021-09-22 15:03:35 +0900

    rename

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use log::info;
 
-pub mod runtime;
+pub mod mirai;
 
 fn main() -> Result<(), anyhow::Error> {
   env_logger::init_from_env(
     env_logger::Env::from(env_logger::Env::default())
       .default_filter_or("info"));
-  let mut rt = runtime::Runtime::new();
+  let mut rt = mirai::runtime::new();
   let r = rt.block_on(async {
     let k = async { 1 };
     10 + k.await
--- src/mirai/runtime.rs
+++ src/mirai/runtime.rs
@@ -1,34 +1,36 @@
-use std::future::{Future};
-use std::task::{Context, Poll, Waker};
 use std::fmt::Debug;
+use std::pin::Pin;
+use std::future::Future;
+use std::task::{Context, Poll, Waker};
+
 use log::info;
+
+pub fn new() -> Runtime {
+  Runtime {
+  }
+}
 
 pub struct Runtime {
 }
 
 impl Runtime {
-  pub fn new() -> Self {
-    Runtime {
-    }
-  }
-
   pub fn block_on<F, R>(&mut self, f: F) -> R
     where
       F: Future<Output=R> + Send,
       R: Debug,
   {
     let waker: Waker = futures::task::noop_waker();
-    let mut context = Context::from_waker(&waker);
-    let mut b = Box::pin(f);
+    let mut context:Context = Context::from_waker(&waker);
+    let mut b: Pin<Box<F>> = Box::pin(f);
     loop {
       let r = b.as_mut().poll(&mut context);
       match r {
         Poll::Pending => {
-          info!("Pending....");
+          info!("Pending");
           continue;
         }
         Poll::Ready(r) => {
-          info!("Ready!: {:?}", r);
+          info!("Ready: {:?}", r);
           return r;
         }
       }
